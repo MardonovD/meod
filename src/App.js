4de7303750d8836c4111a87bfe5fb3dfe95454f1
Dostonbek <mardonovdoston69@gmail.com>
2022-06-03 10:27:41 +0300

    create dolzarb xabarlar kun.uz

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./style/main.scss";
 import Header from "./components/Header";
 import HeaderCount from "./components/HeaderCount";
 import MainSection from "./components/MainSection";
-import Aside from "./components/Aside";
+import DolzarbNew from "./components/DolzarbNew";
 function App() {
   return (
     <div className="app">
       <Header />
       <HeaderCount />
       <MainSection />
+      <DolzarbNew />
     </div>
   );
 }
--- src/components/DolzarbNew.js
+++ src/components/DolzarbNew.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React from "react";
 
 const DolzarbNew = () => {
   return (
-    <div className='dolzarb'>
-      <div className='dolzarb-xabar'>
-        <span>Dolzarb Xabarlar</span>
+    <div className="container-fluid">
+      <div className="dolzarb mt-5">
+        <div className="dolzarb-xabar">
+          <span className="dolzarb-xabar-item">Dolzarb Xabarlar</span>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-6">
+          <div className="dol-left">
+            <div className="dol-left-image">
+              <img
+                src="https://storage.kun.uz/source/thumbnails/_medium/8/ytVfv6e_hRMWDe9C9npvhYRSy8odg9M8_medium.jpg"
+                alt="image dolzarc section"
+              />
+            </div>
+
+            <div className="dol-left-text">
+              <a href="#">
+                Ukraina AQShdan uzoqqa otuvchi raketa tizimlari olmoqda: ular
+                nimani o‘zgartirishi mumkin?
+              </a>
+              <p>
+                Baravar o‘t ochish tizimlari «kumush o‘q» vazifasini bajara
+                oladimi?
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-6"></div>
       </div>
     </div>
   );
 };
-
 
 export default DolzarbNew;
