3dade3a2b0785768bb150f40da47cf38fde59a2d
Dostonbek <mardonovdoston69@gmail.com>
2022-06-03 17:32:40 +0300

    maqolalar finished

--- src/components/Maqolalar.js
+++ src/components/Maqolalar.js
@@ -4,6 +4,9 @@ const Maqolalar = () => {
   return (
     <div className="maqola">
       <div className="container-fluid">
+        <div className="maqola-circl">
+          <span>Maqolalar</span>
+        </div>
         <div className="row">
           <div className="col-4">
             <div className="maqola-image">
@@ -13,7 +16,9 @@ const Maqolalar = () => {
               />
             </div>
             <div className="maqola-text">
-              <a href="3">Urush qonunlari.Ular nimalarga nazarda tutadi?</a>
+              <div className="maqola-text-link">
+                <a href="#">Urush qonunlari.Ular nimalarga nazarda tutadi?</a>
+              </div>
               <p className="maqola-text-p">
                 ​​​​​​​Dunyo tinchlik haqida har qancha bong urmasin eng yaqin
                 qo‘shni, qondosh va bir guruhga mansub ikki millatdan iborat
@@ -21,8 +26,120 @@ const Maqolalar = () => {
               </p>
             </div>
           </div>
-          <div className="col-4"></div>
-          <div className="col-4"></div>
+          <div className="col-4">
+            <div className="maqola-image">
+              <img
+                src="https://storage.kun.uz/source/thumbnails/_medium/8/F18c0DIRlHI9mpVjDZOPLNmfbKX-vZj9_medium.jpg"
+                alt="image"
+              />
+            </div>
+            <div className="maqola-text">
+              <div className="maqola-text-link">
+                <a href="#">
+                  "Bu kasb puxta o'rganishga kamida 4-5 yil kerak" - montyorlik
+                  mashaqqati
+                </a>
+              </div>
+              <p className="maqola-text-p">
+                “Manzil” loyihasi olis hududlarda sokin hayot kechirayotgan
+                oddiy hamyurtlarimiz hayotidan hikoya qilishda davom etadi.
+                Navbatdagi qahramonimiz – xorazmlik Tursunboy aka O‘razboyev 35
+                yildan oshiq umrini montyor sifatida xalqqa elektr energiyasi
+                yetkazib berish uchun sarflagan.
+              </p>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="maqola-image">
+              <img
+                src="https://storage.kun.uz/source/thumbnails/_medium/8/b-jIyrGnyttH3SNw9pD9dUR7DVa2xNd0_medium.jpg"
+                alt="image"
+              />
+            </div>
+            <div className="maqola-text">
+              <div className="maqola-text-link">
+                <a href="#">Oyoqsiz yigit: inson irodasi chegara bilmaydi</a>
+              </div>
+              <p className="maqola-text-p">
+                U ikki oyog‘idan ajralganida sakkiz yoshda edi. Hayotda sinib,
+                o‘rnini topolmay qolishi uchun shu sinovning o‘zi ham kifoya
+                qilardi. Ammo u shaxsga aylanish uchun, birovga muhtoj bo‘lib
+                qolmaslik uchun o‘zini qo‘lga oldi.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="row mt-4">
+          <div className="col-4">
+            <div className="maqola-image">
+              <img
+                src="https://storage.kun.uz/source/thumbnails/_medium/8/qoTzfIFQW3ugqV4qeBR-mxzzoneZ1T3Y_medium.jpg"
+                alt="image"
+              />
+            </div>
+            <div className="maqola-text">
+              <div className="maqola-text-link">
+                <a href="#">
+                  Bu kvartiraning ijara to‘lovi oyiga 30 mln so‘mdan ko‘p -
+                  “Inshoot” loyihasining ilk soni
+                </a>
+              </div>
+              <p className="maqola-text-p">
+                Qurilish va ta’mirlash – xalqimiz eng ko‘p qiziqadigan
+                mavzulardan. Yangi “Inshoot” loyihasi ana shu talabdan kelib
+                chiqib, qurilish trendlari haqida hikoya qiladi.
+              </p>
+            </div>
+          </div>
+
+          <div className="col-4">
+            <div className="maqola-image">
+              <img
+                src="https://storage.kun.uz/source/thumbnails/_medium/8/XUQ0UnV8yvVgbr5sXpilnMqAscAuZ7C5_medium.jpg"
+                alt="image"
+              />
+            </div>
+            <div className="maqola-text">
+              <div className="maqola-text-link">
+                <a href="#">
+                  Dunyoda allaqachon post-JST erasi. Iqtisodiy diplomatiyamiz
+                  faollashishi zarur
+                </a>
+              </div>
+              <p className="maqola-text-p">
+                Dunyo savdo-ta’minot zanjirlarida Jahon savdo tashkiloti
+                imkoniyatlarining maksimum chegarasiga yetib kelgandek
+                ko‘rinmoqda. Oxirgi yillarda tuzilayotgan IPEFP, CPTPP va RCEP
+                kabi ittifoqlar bloklashgan yangi iqtisodiy doiralarni vujudga
+                keltiryapti. Dunyo JSTdan keyingi davrga qadam qo‘yayotgan bir
+                paytda, O‘zbekiston hamon global savdo tizimidan uzilganicha
+                qolmoqda.
+              </p>
+            </div>
+          </div>
+
+          <div className="col-4">
+            <div className="maqola-image">
+              <img
+                src="https://storage.kun.uz/source/thumbnails/_medium/8/tRAP-74GMJsopbHl_2eQ_NGaLgIhLwau_medium.jpg"
+                alt="image"
+              />
+            </div>
+            <div className="maqola-text">
+              <div className="maqola-text-link">
+                <a href="#">
+                  So‘nggi qo‘ng‘iroqdan so‘ng. Bitiruvchilar kasb tanlashda
+                  nimalarni hisobga olishi muhim?
+                </a>
+              </div>
+              <p className="maqola-text-p">
+                Maktablarni 390 mingdan ortiq o‘quvchi tugatyapti. Hozir
+                ularning oldidagi eng katta tanlov kelajakda kim bo‘lishlarini
+                hal qilishdan iborat.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
