77ba375dfd66fda51ab027f2efaa7960e3dfb217
Dostonbek <mardonovdoston69@gmail.com>
2022-06-03 10:16:08 +0300

    create new section

---
+++ src/components/DolzarbNew.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const DolzarbNew = () => {
+  return (
+    <div className='dolzarb'>
+      <div className='dolzarb-xabar'>
+        <span>Dolzarb Xabarlar</span>
+      </div>
+    </div>
+  );
+};
+
+
+export default DolzarbNew;
