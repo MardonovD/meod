57a88b1406cf6eed9c4c8055d599d42986e37b92
Dostonbek <mardonovdoston69@gmail.com>
2022-06-03 13:35:41 +0300

    section start

--- src/components/Intervyu.js
+++ src/components/Intervyu.js
@@ -1,14 +1,25 @@
-import React from 'react';
-
+import React from "react";
 
 const Intervyu = () => {
   return (
-    <div className='intervyu'>
-      
+    <div className="intervyu">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12  d-flex justify-content-between">
+            <div className="intervyu-left">
+              <span>Intervyu</span>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-3"></div>
+          <div className="col-3"></div>
+          <div className="col-3"></div>
+          <div className="col-3"></div>
+        </div>
+      </div>
     </div>
   );
 };
-
-
 
 export default Intervyu;
