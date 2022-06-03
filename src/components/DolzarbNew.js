b0612e14ed26f9329dacf8ee73096970fdef05ee
Dostonbek <mardonovdoston69@gmail.com>
2022-06-03 13:14:14 +0300

    about section kun.uz

--- src/components/DolzarbNew.js
+++ src/components/DolzarbNew.js
@@ -5,7 +5,7 @@ const DolzarbNew = () => {
     <div className="container-fluid">
       <div className="dolzarb mt-5">
         <div className="dolzarb-xabar">
-          <span className="dolzarb-xabar-item">Dolzarb Xabarlar</span>
+          <span className="dolzarb-xabar-item mb-4">Dolzarb Xabarlar</span>
         </div>
       </div>
 
@@ -31,7 +31,65 @@ const DolzarbNew = () => {
             </div>
           </div>
         </div>
-        <div className="col-6"></div>
+
+        <div className="col-6">
+          <div className="row">
+            <div className="col-6">
+              <div className="card-dol d-flex flex-column  gap-2">
+                <img
+                  src="https://storage.kun.uz/source/thumbnails/_medium/8/3qNc_rpVqsCnMTxIzv1YVyxj8d-7_ORx_medium.jpg"
+                  alt="image dol"
+                />
+                <div href="#">
+                  Ikki oy oldin armiyaga kuzatilgan yigit yurak xurujidan
+                  o‘lgani aytilmoqda, marhumning yaqinlari bunga ishonmayapti
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="card-dol d-flex flex-column  gap-2">
+                <img
+                  src="https://storage.kun.uz/source/thumbnails/_medium/8/fxdBWdSSSYkwBpL8Qf9tL2Xf7P8ZoLzi_medium.jpg"
+                  alt="image dol"
+                />
+                <div href="#">
+                  Islom Karimovga yozilgan xat, so‘m-kuponlar, 7 yoshda 3-sinf -
+                  bolalik xotiralari
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-6">
+            <div className="row">
+              <div className="col-6">
+                <div className="card-dol d-flex flex-column  gap-2">
+                  <img
+                    src="https://storage.kun.uz/source/thumbnails/_medium/8/ukCNp8TT2vO3tYK-1lGKP2WJ-0yZ7rG6_medium.jpg"
+                    alt="image dol"
+                  />
+                  <div href="#">
+                    1 iyun – bolalarni himoya qilish kuni. Ularni nimalardan
+                    himoya qilish kerak?
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-6">
+                <div className="card-dol d-flex flex-column  gap-2">
+                  <img
+                    src="https://storage.kun.uz/source/thumbnails/_medium/8/HKuckJ5-r2_tgDNZMO20KEWVOTwuE_CL_medium.jpg"
+                    alt="image dol"
+                  />
+                  <div href="#">
+                    30 yillik kechikishning oqibatlarini yana kechiktirish bilan
+                    davolab bo‘lmaydi - Otabek Bakirov
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
