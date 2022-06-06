7315a378ef56b0c1470c7032913e22e36a42dad5
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-06 14:07:00 +0300

    responsive for mixin in sass header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = () => {
-// git brancg bilan yangi branchga utdik barnch nomi doston 
+  // git brancg bilan yangi branchga utdik barnch nomi doston
   return (
     <div className="header">
       <div className="container-fluid">
@@ -40,6 +40,23 @@ const Header = () => {
                 </li>
                 <li className="nav-item">
                   <a href="#">AUDIO </a>
+                </li>
+                <li className="nav-item ">
+                  <div className="icon-menu ">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="30"
+                      height="30"
+                      fill="currentColor"
+                      className="bi bi-list"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+                      />
+                    </svg>
+                  </div>
                 </li>
               </ul>
             </nav>
