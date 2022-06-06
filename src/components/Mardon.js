bb8736ddc95560db6b391ea2baa1454d4ed6b47a
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-06 09:39:12 +0300

    create mardon.js

---
+++ src/components/Mardon.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Mardon = () => {
+  return (
+    <div>
+      <h1>Hello Guys!</h1>
+    </div>
+  );
+};
+
+Mardon.propTypes = {};
+
+export default Mardon;
